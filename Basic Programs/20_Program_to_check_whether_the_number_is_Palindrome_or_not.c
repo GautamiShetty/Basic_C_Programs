49f70f976f61191a5199bdd35f88bf5f1f9b79fa
GautamiShetty <95223703+GautamiShetty@users.noreply.github.com>
2022-02-12 00:06:25 +0530

    Add files via upload

---
+++ Basic Programs/20_Program_to_check_whether_the_number_is_Palindrome_or_not.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num = 0, revNum = 0, temp = 0;
+    printf("\nHii");
+
+    printf("Enter the number: \n");
+    scanf("%d",&num);
+    
+    num = temp;
+    while(temp > 0)
+    {
+        revNum = (revNum*10) + (temp%10);
+        temp /= 10;
+    }    
+
+    if(num == revNum)
+    {
+        printf("%d is palindrome number", num);
+    }
+    else
+    {
+        printf("%d is not palindrome number", num);
+    }
+    getch();
+    return 0;
+}
