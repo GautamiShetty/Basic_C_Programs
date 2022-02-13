8080700495c7e0fffdfb4d7ee3978341c09e17cb
GautamiShetty <95223703+GautamiShetty@users.noreply.github.com>
2022-02-13 16:48:52 +0530

    Add files via upload

---
+++ Logical_Building/Assignment_No_1/05_Program_to_accept_a_number_from_user_and_display_that_number_of_asterisk_symbol_on_screen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+
+void asterisk(int ino)
+{
+    if(ino < 0)
+    {
+        printf("\nInvalid number!!!");
+    }
+
+    putchar('\n');
+
+    while(ino > 0)
+    {
+        printf(" * ");
+        ino--;
+    }
+
+}
+
+int main()
+{
+
+    int inum = 0;
+
+    printf("\nEnter the number : ");
+    scanf("%d",&inum);
+    asterisk(inum);
+	getch();
+	return 0;
+}
